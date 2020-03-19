30786284d46053cd23d3cb14f1fc3f98ace66820
kharioki <antolisah@gmail.com>
2020-03-20 02:00:23 +0300

    dynamic css

--- app.js
+++ app.js
@@ -1,49 +1,18 @@
 new Vue({
     el:'#vue-app',
     data: {
-        // name: '',
-        age: 20,
-        // x: 0,
-        // y: 0
-        a: 0,
-        b: 0
+        available: false,
+        nearby: false
     },
     methods: {
-        // add: function(inc) {
-        //     this.age += inc;
-        // },
-        // subtract: function(dec) {
-        //     this.age -= dec;
-        // },
-        // updateXY: function(event) {
-        //     this.x = event.offsetX;
-        //     this.y = event.offsetY;
-        // },
-        // click: function() {
-        //     alert('You clicked me!')
-        // }
-        // logName: function() {
-        //     console.log('You entered your name');
-        // },
-        // logAge: function() {
-        //     console.log('You entered your age');
-        // }
-
-        // addToA: function() {
-        //     return this.a + this.age
-        // },
-        // addToB: function() {
-        //     return this.b + this.age
-        // }
+        
     },
     computed: {
-        addToA: function() {
-            console.log('addToA');
-            return this.a + this.age
-        },
-        addToB: function() {
-            console.log('addToB');
-            return this.b + this.age
+        compClasses: function() {
+            return {
+                available: this.available,
+                nearby: this.nearby
+            }
         }
     }
 });
