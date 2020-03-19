a8f3b6008459c07929188829fe63a31876097e63
kharioki <antolisah@gmail.com>
2020-03-19 23:50:11 +0300

    data binding

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 new Vue({
     el:'#vue-app',
     data: {
-        name: 'Kharioki'
+        name: 'Kharioki',
+        job: 'Avenger',
+        website: 'https://about.me/kharioki',
+        websiteTag: '<a href="https://about.me/kharioki">Khariokis website</a>'
+    },
+    methods: {
+        greet: function(time){
+            return 'Good ' + time + ' ' + this.name
+        }
     }
 });
